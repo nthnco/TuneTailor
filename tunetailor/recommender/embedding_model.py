615bf4381304e5fb113f2aaf49c73c01d0f9add3
nthnco <nathanreso99@gmail.com>
2025-07-26 21:54:16 -0700

    add get_user_embedding and change normalize_features -> normalized_tempo

--- tunetailor/recommender/embedding_model.py
+++ tunetailor/recommender/embedding_model.py
@@ -1,9 +1,9 @@
 import numpy as np
 from typing import List, Dict, Any
 
-def normalize_features(features: Dict[str, float]) -> np.ndarray:
+def normalize_tempo(features: Dict[str, float]) -> np.ndarray:
     """
-    Normalize function
+    Normalize the tempo feature, all other features should already be between 0-1
 
     Args:
         features (Dict[str, float]): _description_
@@ -16,8 +16,21 @@ def normalize_features(features: Dict[str, float]) -> np.ndarray:
     features['tempo'] = (bpm - 60) / (200 - 60)
     for i in features.values():
         normalized_features.append(i)
-    return np.array(normalized_features)
+    return np.array(list(normalized_features))
     
 def get_user_embedding(liked_tracks: List[Dict[str, Any]]) -> np.ndarray:
-    # YOUR CODE HERE  
-    pass
+    """
+    create a numerical representation of the 
+    user's music taste by analyzing their liked tracks.
+    Args:
+        liked_tracks (List[Dict[str, Any]]): all liked tracks from user
+
+    Returns:
+        np.ndarray: user embedding vector of overall music tastes
+    """
+    all_vectors = []
+    for i in liked_tracks:
+        normalized_arr = normalize_tempo(i)
+        all_vectors.append(normalized_arr)
+    return np.mean(np.array(all_vectors), axis = 0)
+    
